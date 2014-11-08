ff32244be0065746d6764fd901e2cc4f783bb257
Randall Degges <r@rdegges.com>
2014-11-07 16:27:13 -0800

    b

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(stormpath.init(app, {
   apiKeyId:     process.env.STORMPATH_API_KEY_ID,
   apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
   application:  process.env.STORMPATH_URL,
+  secretKey:    process.env.STORMPATH_SECRET_KEY,
 }));
 
 //app.use(routes);
