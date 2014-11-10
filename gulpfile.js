088767b3a633ac84f4c23308a1b83f29bd04f590
Randall Degges <r@rdegges.com>
2014-11-10 11:28:01 -0800

    Adding basic gulpfile.
    Right now it only handles css stuffz.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var bower = require('gulp-bower');
+var gulp = require('gulp');
+var liveReload = require('gulp-livereload');
+var stylus = require('gulp-stylus');
+
+var DEV_MODE = (process.env.NODE_ENV ? process.env.NODE_ENV === 'development' : false);
+
+/**
+ * Compile all of our Stylus CSS templates into proper CSS files.
+ *
+ * If this app is in development mode, CSS will be pretty-printed -- otherwise,
+ * CSS will be compressed.
+ */
+gulp.task('css', function() {
+  gulp.src('./assets/css/*.styl')
+    .pipe(stylus({ compress: DEV_MODE }))
+    .pipe(gulp.dest('./assets/css/'));
+});
