4bdd8954633eeaa9dcbbac81ff6526413db83ee3
Randall Degges <r@rdegges.com>
2014-11-10 14:03:12 -0800

    Adding a new `gulp run` task.
    This runs our development flow.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var bower = require('gulp-bower');
 var del = require('del');
 var gulp = require('gulp');
 var liveReload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
 var stylus = require('gulp-stylus');
 
 var DEV_MODE = (process.env.NODE_ENV || 'development') === 'development';
@@ -31,4 +32,34 @@ gulp.task('cleanBower', function(cb) {
 gulp.task('bower', ['cleanBower'], function() {
   bower()
     .pipe(gulp.dest('./assets/bower'));
+});
+
+/**
+ * Run our local development server, and watch for changes.
+ */
+gulp.task('run', ['bower', 'css'], function() {
+  liveReload.listen();
+
+  // If any new Bower components are installed -- copy them over into assets.
+  gulp.watch('./bower_components/**', ['bower']);
+
+  // If any Bower packages change, reload the live server.
+  gulp.watch('./assets/bower/**').on('change', liveReload.changed);
+
+  // If any stylus files are changed, recompile the CSS.
+  gulp.watch('./assets/css/*.styl', ['css']).on('change', liveReload.changed);
+
+  // If any JS files are changed, restart the Node server.
+  nodemon({
+    script: 'index.js',
+    ext: 'js jade',
+    ignore: ['gulpfile.js', 'node_modules/**', 'bower_components/**'],
+    env: {
+      NODE_ENV: process.env.NODE_ENV || 'development',
+    },
+  })
+    .on('change', liveReload.changed)
+    .on('restart', function() {
+      console.log('Restarted!');
+    });
 });
